01b94bcad72fe30fe3e101ef1c88cbc35287bc37
chengdai <chengdai.dev@gmail.com>
2024-07-03 15:43:32 +0200

    updated style

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,17 @@ const App = () => {
   const converter = OpenCC.Converter({ from: "cn", to: "tw" });
 
   return (
-    <div className="p-4 ">
-      <h1 className="text-xl font-semibold text-center py-4">簡體轉繁體</h1>
-      <textarea
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        className="border-2 border-black/20 rounded-md w-full h-[30vh] p-4"
-      />
-      <pre>{converter(text)}</pre>
+    <div className="">
+      <main className="p-4">
+        <h1 className="text-xl font-semibold text-center py-4">簡體轉繁體</h1>
+        <textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="border-2 border-black/20 rounded-md w-full h-[30vh] p-4"
+        />
+        <pre>{converter(text)}</pre>
+      </main>
+
       <Footer />
     </div>
   );
--- src/conponents/Footer.tsx
+++ src/conponents/Footer.tsx
@@ -1,16 +1,16 @@
 const Footer = () => {
   return (
-    <p className="fixed bottom-2  w-full text-center font-mono text-sm">
+    <p className="fixed w-full bottom-0 py-1 text-white/80  text-center font-mono text-sm bg-slate-500">
       {" "}
       Powered by{" "}
       <a
         href="https://github.com/BYVoid/OpenCC"
-        className="text-blue-500 hover:underline"
+        className="text-blue-300 hover:underline"
       >
         OpenCC
       </a>{" "}
       &{" "}
-      <a href="https://chengdai.dev" className="text-blue-500 hover:underline">
+      <a href="https://chengdai.dev" className="text-blue-300 hover:underline">
         Cheng
       </a>
     </p>
