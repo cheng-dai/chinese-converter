4aa66f46f19a6b38d8c8946f1a8ab132fa024a8e
chengdai <chengdai.dev@gmail.com>
2024-07-03 15:29:59 +0200

    updated style

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ const App = () => {
   const converter = OpenCC.Converter({ from: "cn", to: "tw" });
 
   return (
-    <div>
-      <h1>簡體轉繁體</h1>
+    <div className="p-4 ">
+      <h1 className="text-xl font-semibold text-center py-4">簡體轉繁體</h1>
       <textarea
         value={text}
         onChange={(e) => setText(e.target.value)}
-        rows={20}
+        className="border-2 border-black/20 rounded-md w-full h-[30vh] p-4"
       />
       <pre>{converter(text)}</pre>
     </div>
