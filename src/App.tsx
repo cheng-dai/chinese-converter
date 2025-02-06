01c3e9b4fc69878b3635df7895d338f23f2380fb
chengdai <chengdai.dev@gmail.com>
2025-02-06 14:38:51 +0100

    Add switch function

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,45 @@ import Footer from "./conponents/Footer";
 
 const App = () => {
   const [text, setText] = useState("");
-  const converter = OpenCC.Converter({ from: "cn", to: "tw" });
+  const [toTraditional, setToTraditional] = useState(true);
+  const converter = toTraditional
+    ? OpenCC.Converter({ from: "cn", to: "tw" })
+    : OpenCC.Converter({ from: "tw", to: "cn" });
 
   return (
-    <div className="">
+    <div className="min-h-screen">
       <main className="p-4">
-        <h1 className="text-xl font-semibold text-center py-4">簡體轉繁體</h1>
+        <div className="flex justify-center items-center gap-4">
+          <h1 className="text-xl font-semibold text-center py-4">
+            {toTraditional ? "简体" : "繁體"}
+          </h1>
+          <button
+            className="border-2 border-black/20 rounded-md px-2 py-1 hover:bg-black/10"
+            onClick={() => setToTraditional(!toTraditional)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill="currentColor"
+                d="M5.22 14.78a.75.75 0 0 0 1.06-1.06L4.56 12h8.69a.75.75 0 0 0 0-1.5H4.56l1.72-1.72a.75.75 0 0 0-1.06-1.06l-3 3a.75.75 0 0 0 0 1.06zm5.56-6.5a.75.75 0 1 1-1.06-1.06l1.72-1.72H2.75a.75.75 0 0 1 0-1.5h8.69L9.72 2.28a.75.75 0 0 1 1.06-1.06l3 3a.75.75 0 0 1 0 1.06z"
+              />
+            </svg>
+          </button>
+          <h1 className="text-xl font-semibold text-center py-4">
+            {toTraditional ? "繁體" : "简体"}
+          </h1>
+        </div>
         <textarea
           value={text}
+          placeholder={
+            toTraditional ? "请输入要转换的文字" : "請輸入要轉換的文字"
+          }
           onChange={(e) => setText(e.target.value)}
-          className="border-2 border-black/20 rounded-md w-full h-[30vh] p-4"
+          className="border-2 border-black/20 rounded-md w-full h-[30vh] p-4 focus:outline-none  "
         />
         <pre>{converter(text)}</pre>
       </main>
